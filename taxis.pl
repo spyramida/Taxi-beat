c7a62d5b4a8c7ec4e123fb7a1b69dd6c28568306
spyramida <spyros-papp@hotmail.com>
2019-09-01 23:12:29 +0300

    Create taxis.pl

---
+++ taxis.pl
@@ -0,0 +1,17 @@
+taxi(23.741587,37.984125,100,yes,1,4,greek,9.2,yes,subcompact).
+taxi(23.691133,37.967339,110,yes,1,4,greek,8.0,yes,large).
+taxi(23.691133,37.967339,110,yes,1,4,english,8.0,yes,large).
+taxi(23.757838,37.962517,120,yes,1,4,greek,9.2,yes,compact).
+taxi(23.789114,38.032908,130,no,1,4,greek,8.3,yes,subcompact).
+taxi(23.789114,38.032908,130,no,1,4,english,8.3,yes,subcompact).
+taxi(23.766993,38.018644,140,yes,1,4,greek,9.1,no,large).
+taxi(23.766993,38.018644,140,yes,1,4,english,9.1,no,large).
+taxi(23.734153,37.961316,150,yes,1,4,greek,6.3,no,subcompact).
+taxi(23.740078,37.945631,160,yes,1,4,greek,7.1,yes,large).
+taxi(23.740078,37.945631,160,yes,1,4,english,7.1,yes,large).
+taxi(23.756566,37.997309,170,no,1,4,greek,8.9,yes,large).
+taxi(23.756566,37.997309,170,no,1,4,english,8.9,yes,large).
+taxi(23.767763,37.945429,180,yes,1,4,greek,5.0,no,subcompact).
+taxi(23.689228,38.010435,190,yes,1,4,greek,7.2,no,large).
+taxi(23.689228,38.010435,190,yes,1,4,english,7.2,no,large).
+taxi(23.795044,38.012519,200,yes,1,4,greek,8.0,no,compact).
